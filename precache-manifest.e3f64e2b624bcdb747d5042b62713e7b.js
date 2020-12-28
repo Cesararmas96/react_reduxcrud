d7975c436551ee4203daa762460ccf1eb51e3385
cesararmas96 <caal2096@gmail.com>
2020-12-28 16:18:26 -0430

    Updates

---
+++ precache-manifest.e3f64e2b624bcdb747d5042b62713e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f68fba15fa43cd507ac6e389e67811",
+    "url": "/react_reduxcrud/index.html"
+  },
+  {
+    "revision": "83e0b09d1a91d247e943",
+    "url": "/react_reduxcrud/static/css/main.dc977176.chunk.css"
+  },
+  {
+    "revision": "28e93ed3fd8205d0ae16",
+    "url": "/react_reduxcrud/static/js/2.4e38b94f.chunk.js"
+  },
+  {
+    "revision": "8dc647c3672b30664d97feee03a4be2d",
+    "url": "/react_reduxcrud/static/js/2.4e38b94f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83e0b09d1a91d247e943",
+    "url": "/react_reduxcrud/static/js/main.514b898c.chunk.js"
+  },
+  {
+    "revision": "9e9396e18516ee0c41a0",
+    "url": "/react_reduxcrud/static/js/runtime-main.e3307c7c.js"
+  }
+]);
